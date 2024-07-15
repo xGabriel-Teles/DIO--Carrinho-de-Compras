1e9a582b56e4295e2a09e312ea77cdec79e91c4d
xGabriel-Teles <gabriel.teles.santos@hotmail.com>
2024-07-15 15:30:19 -0300

    add cart and item.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+import createItem from "./services/item.js"; 
+
+const cart = [];
+
+console.log('Welcome to the your Shopee Cart!')
+
+const item1 = await createItem('Hotwheels ferrari', 20.99, 1)
+const item2 = await createItem('Hotwheels lamborghini', 39.99, 3)
+
+console.log(item2.subtotal())
---
+++ src/services/cart.js
@@ -0,0 +1,20 @@
+// quais ações meu carrinho pode fazer?
+
+
+//CASOS DE USO
+// --> adicionar item 
+async function addItem(userCart){
+
+}
+// --> deletar item do carrinho
+async function deleteItem(userCart, name){
+
+}
+// --> remover um item // diminui quantidade
+async function removeItem(userCart, index){
+
+}
+// --> calcular o total
+async function calculateTotal(userCart){
+
+}
